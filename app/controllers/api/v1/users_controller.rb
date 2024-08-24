73eecdeae7ca37a7e351e3fb972e46a40315e825
mehmetalemdar <alemdarmehmet6@gmail.com>
2024-08-24 15:45:00 +0300

    Initial commit

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    if params[:user].nil? || params[:user][:username].nil? || params[:user][:password].nil?
+      render json: { error: 'Username and password are required' }, status: :unprocessable_entity
+    else
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
